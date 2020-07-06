64f94ee136a525db68ad926d274ceca654487f1a
jgusal <jorge.g@usal.es>
2020-07-06 21:53:18 +0200

    Incluir scripts de instalacion de Kubernetes

---
+++ Kubernetes/install_kuber.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+set -e
+apt update
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt update
+apt install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+kubeadm config images pull
